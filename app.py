a5eb87228c6c73689f84ff2f4f38c9f44a66f321
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-03-09 17:56:28 +0530

    Made upload function

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app: Flask = Flask(__name__)
@@ -7,6 +7,18 @@ app: Flask = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    file = request.files["file"]
+    print(type(file))
+
+    if not file:
+        return "No File Selected."
+
+    file.save(f"uploads/idk_the_name")
+    return "File Saved"
 
 
 if __name__ == "__main__":
